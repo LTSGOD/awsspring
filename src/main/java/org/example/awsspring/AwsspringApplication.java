0369c91b1fc6bcda599f7b95ae8f2f975a24d8b6
LTSGOD <lts0424@naver.com>
2023-01-22 15:14:11 +0900

    Initial commit

---
+++ src/main/java/org/example/awsspring/AwsspringApplication.java
@@ -0,0 +1,13 @@
+package org.example.awsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/awsspring/AwsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
