045fd9a499dd14f6a446c9d45c8008a4dff4c1af
LTSGOD <lts0424@naver.com>
2023-01-24 20:25:16 +0900

    3장까지 공부 내용

---
+++ src/test/java/org/example/awsspring/web/HelloControllerTest.java
@@ -0,0 +1,46 @@
+package org.example.awsspring.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void hello가_리턴된다() throws Exception{
+		String hello = "hello";
+
+		mvc.perform(get("/hello"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(hello));
+
+	}
+
+	@Test
+	public void HelloDto가_리턴된다() throws Exception{
+		//given
+		String name = "taesoon";
+		int amount = 1000;
+
+		//when
+		mvc.perform(
+					get("/hello/dto")
+							.param("name", name)
+							.param("amount",String.
+								valueOf(amount)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name", is(name)))
+			.andExpect((jsonPath("$.amount",is(amount))));
+	}
+}
