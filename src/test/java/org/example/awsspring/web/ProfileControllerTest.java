49902b4345623f69380dee64329302d48f2849ca
LTSGOD <lts0424@naver.com>
2023-02-23 22:49:36 +0900

    10.3장 완ㄹ

---
+++ src/test/java/org/example/awsspring/web/ProfileControllerTest.java
@@ -0,0 +1,59 @@
+package org.example.awsspring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProfileControllerTest {
+    @Test
+    public void get_real_profile() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void getFirstWhenNo_real_profile() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void getDefaultWhenNo_active_profile() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
